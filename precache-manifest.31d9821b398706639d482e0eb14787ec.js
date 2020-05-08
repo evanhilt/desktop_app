efe13447eeb5d563305332f9ad50a2d26d06bcfc
Evan Hilton <evanhilton@Evans-Air.hsd1.ut.comcast.net>
2020-05-08 14:59:33 -0600

    Updates

---
+++ precache-manifest.31d9821b398706639d482e0eb14787ec.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "376eb538e79011be0a81",
+    "url": "/desktop_app/static/css/main.6de24262.chunk.css"
+  },
+  {
+    "revision": "376eb538e79011be0a81",
+    "url": "/desktop_app/static/js/main.376eb538.chunk.js"
+  },
+  {
+    "revision": "ed7648a3b6a046882657",
+    "url": "/desktop_app/static/js/1.ed7648a3.chunk.js"
+  },
+  {
+    "revision": "4ec9e51cfea02ff596c9",
+    "url": "/desktop_app/static/js/runtime~main.4ec9e51c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/desktop_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ba01b6345036c18cfac18f41292a55f",
+    "url": "/desktop_app/static/media/arrow.2ba01b63.svg"
+  },
+  {
+    "revision": "3069df4e5cc00e3fac3409f751644ca3",
+    "url": "/desktop_app/static/media/sunny.3069df4e.svg"
+  },
+  {
+    "revision": "fac1f0db6f921e8c2c3f1a04b911d9ca",
+    "url": "/desktop_app/static/media/partly_cloudy.fac1f0db.svg"
+  },
+  {
+    "revision": "41dc88e93756d8335f75d325beb5d191",
+    "url": "/desktop_app/static/media/cloudy.41dc88e9.svg"
+  },
+  {
+    "revision": "106a30f39c31086edd26725550228b69",
+    "url": "/desktop_app/static/media/rain.106a30f3.svg"
+  },
+  {
+    "revision": "9470a13bcacf74241f55c5f58df89dac",
+    "url": "/desktop_app/static/media/sleet.9470a13b.svg"
+  },
+  {
+    "revision": "64f133dff6ef8fb52bb6eac99e888255",
+    "url": "/desktop_app/static/media/snow.64f133df.svg"
+  },
+  {
+    "revision": "70b75120012a2bc8c0edf506094b2ffc",
+    "url": "/desktop_app/static/media/hail.70b75120.svg"
+  },
+  {
+    "revision": "23a912b06f0bc2721946713c9f050951",
+    "url": "/desktop_app/static/media/none.23a912b0.svg"
+  },
+  {
+    "revision": "abc97d1edee7b48404061ac951771918",
+    "url": "/desktop_app/index.html"
+  }
+];
